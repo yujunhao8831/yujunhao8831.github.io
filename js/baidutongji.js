8eb3c5ba650801e97b7e2caaac129abbfc39b916
yujunhao <yujunhao@zsyjr.com>
2020-06-18 09:58:42 +0800

    Site updated: 2020-06-18 09:58:37

---
+++ js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?506781d5e40917565f749b6045dd4780";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
